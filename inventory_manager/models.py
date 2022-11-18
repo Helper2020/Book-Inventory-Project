4673d3aceb60e659b68c2ffed0a95e24fd55e7a0
Gabriel Fuentes <12247834+Helper2020@users.noreply.github.com>
2022-11-18 17:25:53 -0500

    Genre model created

--- inventory_manager/models.py
+++ inventory_manager/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.urls import reverse 
 
 # Create your models here.
+"""
+Genre model: This model represents a particular genres.
+"""
+class Genre(models.Model):
+    # Autofield by django is 'id
+    name = models.CharField(max_length=100, help_text='Enter a book genre')
+
+    def get_absolute_url(self):
+        return reverse('genre-info', args=[str(self.id)])
+
+    def __str__(self):
+        return self.name
+
+
