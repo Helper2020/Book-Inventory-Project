a59b4d535dded160c864344a1298da672891da53
Gabriel Fuentes <12247834+Helper2020@users.noreply.github.com>
2022-11-19 17:58:36 -0500

    Created author model

--- inventory_manager/models.py
+++ inventory_manager/models.py
@@ -3,11 +3,11 @@ from django.urls import reverse
 
 # Create your models here.
 """
-Genre model: This model represents a particular genres.
+Genre model: This model represents a genre.
 """
 class Genre(models.Model):
     # Autofield by django is 'id
-    name = models.CharField(max_length=100, help_text='Enter a book genre')
+    name = models.CharField(max_length=100, help_text='Enter a book genre.')
 
     def get_absolute_url(self):
         return reverse('genre-info', args=[str(self.id)])
@@ -15,4 +15,18 @@ class Genre(models.Model):
     def __str__(self):
         return self.name
 
+"""
+Author model: This model represents an author.
+"""
+class Author(models.Model):
+    first_name = models.CharField(max_length=100, help_text='Enter first name of author.')
+    last_name = models.CharField(max_length=100, help_text='Enter last name of author.')
 
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def get_absolute_url(self):
+        return reverse('Author-info', args=[str(self.id)])
+
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
