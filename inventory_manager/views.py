1eb16ad8c9ea3b327f2e119a919e988fcfe76831
Gabriel Fuentes <12247834+Helper2020@users.noreply.github.com>
2022-11-25 16:58:17 -0500

    Added url /view index

--- inventory_manager/views.py
+++ inventory_manager/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Book, Author, SupportTicket, Genre
 
 # Create your views here.
+
+def index(request):
+    num_books = Book.objects.all().count()
+    num_genres = Genre.objects.all().count()
+    num_authors =  Author.objects.all().count()
+
+    info = {
+        'num_books': num_books,
+        'num_authors': num_authors,
+        'num_genres': num_genres
+    }
+    
+    return render(request, 'book_manager/index.html', context=info)
