e984b7cab06c1f42cfa407db2aeee6e08763a3aa
Gabriel Fuentes <12247834+Helper2020@users.noreply.github.com>
2022-11-27 16:24:49 -0500

    created bookcatalog  template and bookinfo page

--- inventory_manager/urls.py
+++ inventory_manager/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
      path('', views.index, name='index'),
+     path('book_catalog/', views.BookCatalog.as_view(), name='book-catalog'),
+     path('book_catalog/<int:pk>', views.BookInfo.as_view(), name='book-info'),
 ]
