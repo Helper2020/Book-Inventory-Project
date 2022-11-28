aaa8e8d3c56bf7ec12c4b8da548a0aa55db29660
Gabriel Fuentes <12247834+Helper2020@users.noreply.github.com>
2022-11-28 14:13:25 -0500

    author catalog/info views created

--- inventory_manager/urls.py
+++ inventory_manager/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
      path('', views.index, name='index'),
      path('book_catalog/', views.BookCatalog.as_view(), name='book-catalog'),
      path('book_catalog/<int:pk>', views.BookInfo.as_view(), name='book-info'),
+     path('author_catalog/', views.AuthorCatalog.as_view(), name='author-catalog'),
+      path('author_catalog/<int:pk>', views.AuthorInfo.as_view(), name='author-info'),
 ]
