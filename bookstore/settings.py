1855d0cc3105b742494f4abb3a85f11316358936
Gabriel Fuentes <12247834+Helper2020@users.noreply.github.com>
2022-11-28 23:48:34 -0500

    Added login and logout function

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'bookstore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -143,3 +143,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# redirect user afer successful login
+LOGIN_REDIRECT_URL = "/"
