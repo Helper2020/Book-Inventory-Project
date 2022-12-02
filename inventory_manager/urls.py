34dc645cb16ace1e9f51ea5d69949d691748f4c5
Gabriel Fuentes <12247834+Helper2020@users.noreply.github.com>
2022-12-02 17:26:23 -0500

    Author Managent created

--- inventory_manager/urls.py
+++ inventory_manager/urls.py
@@ -7,6 +7,10 @@ urlpatterns = [
      path('book_catalog/<int:pk>', views.BookInfo.as_view(), name='book-info'),
      path('author_catalog/', views.AuthorCatalog.as_view(), name='author-catalog'),
      path('author_catalog/<int:pk>', views.AuthorInfo.as_view(), name='author-info'),
+     path('author_management/', views.author_management, name='author-management'),
+     path('author_management/create-author', views.create_author, name='create-author'),
+     path('author_management/author-results', views.SearchAuthorResultsView.as_view(), name='author-results'),
+     path('author_management/update-author/<int:pk>', views.UpdateAuthor.as_view(), name='update-author'),
      path('basic-author-results', views.BasicAuthorResultsView.as_view(), name='basic-author-results'),
      path('basic-book-results', views.BasicBookResultsView.as_view(), name='basic-book-results'),
 ]
